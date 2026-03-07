7683b01e27ac9a1e641e6d9e1717fed4d32d747a
Anujaa C <anujaac0624@gmail.com>
2026-03-08 01:48:35 +0530

    UC9 Code:Recursive Palindrome Checker

---
+++ src/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,32 @@
+public class UseCase9PalindromeCheckerApp {
+
+    // Recursive method
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        // Base condition
+        if (start >= end) {
+            return true;
+        }
+
+        // Check characters
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        boolean result = isPalindrome(word, 0, word.length() - 1);
+
+        if (result) {
+            System.out.println("The given string is a palindrome.");
+        } else {
+            System.out.println("The given string is not a palindrome.");
+        }
+    }
+}
